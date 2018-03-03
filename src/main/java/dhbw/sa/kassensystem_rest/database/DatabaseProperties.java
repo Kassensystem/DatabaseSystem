f544d476a3cd31a4d5495825f247e6bf34baf1ac
marvinmai <marvinmai@posteo.de>
2018-03-03 18:24:44 +0100

    DatabaseProperties_abstract entfernt

--- src/main/java/dhbw/sa/kassensystem_rest/database/DatabaseProperties.java
+++ src/main/java/dhbw/sa/kassensystem_rest/database/DatabaseProperties.java
@@ -7,9 +7,29 @@ package dhbw.sa.kassensystem_rest.database;
  *
  * @author Marvin Mai
  */
-class DatabaseProperties extends DatabaseProperties_abstract {
+class DatabaseProperties
+{
+    private String url = "jdbc:mysql://localhost:3306/?verifyServerCertificate=false&useSSL=true";
+    private String database = "Kassensystem";
+    private static String username;
+    private static String password;
 
     DatabaseProperties() {
-        super();
+        this.username = "DatabaseService";
+        this.password = "password";
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getDatabase() { return this.database; }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
